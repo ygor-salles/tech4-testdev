1e67c87da0e4cfe01b2b845b88641f9877f29175
unknown <ygorsallesaniceto@gmail.com>
2022-01-11 21:10:50 -0300

    router welcome

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import { HistoryController } from '../src/app/controllers/HistoryController'
  
 const router = Router();
 const historyController = new HistoryController();
+
+router.post('/', (req: Request, res: Response) => {
+    res.json({ message: 'Welcome api-tech4' })
+})
 
 router.post('/history', historyController.create);
 router.get('/history/topFive', historyController.readTopFiveSearchedCities)
